487ea0013e1d9afdeba501586295cb06b26a99f8
Vishwa Wijesakare <agvawijesekara@std.appsc.sab.ac.lk>
2024-12-22 21:57:15 +0530

    New update

--- src/home.js
+++ src/home.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import './homepage.css';
+import './login.css';
 
-const HomePage = () => {
+
   return (
-    <div className="homepage-container">
-      <div className="homepage-box">
-        <h1 className="homepage-heading">Welcome to SkillSwap!</h1>
-        <p className="homepage-subheading">Where skills meet opportunity</p>
-        <div className="homepage-actions">
-          <button className="homepage-button" onClick={() => alert('Explore Features!')}>
-            Explore Features
-          </button>
-          <button className="homepage-button" onClick={() => alert('Start Connecting!')}>
-            Start Connecting
-          </button>
-        </div>
-        <p className="homepage-footer">
-          Need help? Visit our <a href="/help.js">Help Center</a>
+    <div className="login-container">
+      <div className="login-box">
+        <h1 className="login-heading">Welcome to SkillSwap</h1>
+        <p className="login-subheading">Connect - Learn - Grow</p>
+
+        {error && <div className="error-message">{error}</div>}
+
+        <form className="login-form" onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              placeholder="Enter your email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              placeholder="Enter your password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+
+          <button type="submit" className="login-button">LOG IN</button>
+        </form>
+
+        <p className="signup-prompt">
+          Don't have an account? <a href="/signup.js">Sign up here</a>
         </p>
       </div>
     </div>
   );
-};
 
-export default HomePage;
+
+export default Login;
